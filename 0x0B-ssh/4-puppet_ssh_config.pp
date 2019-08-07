f05859084d1186785ef654dec37627b662165d94
javb92 <mentor37_@hotmail.com>
2019-08-07 02:26:45 +0000

    first try

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# create a file in tmp folder
+file { 'file':
+  ensure => absent,
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication yes',
+}
+-> file_line { 'Find Identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^IdentityFile.*',
+}
