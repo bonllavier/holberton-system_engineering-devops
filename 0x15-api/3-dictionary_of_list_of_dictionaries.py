708817cde5538f47703851fedfbac53d81e347ed
javb92 <mentor37_@hotmail.com>
2019-09-17 05:22:15 +0000

    fixed permission

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,24 +8,21 @@ if __name__ == "__main__":
     numbertask = 0
     taskcomplete = 0
     tasksdone = []
-    userid = sys.argv[1]
     url1 = 'https://jsonplaceholder.typicode.com/todos'
     with urllib.request.urlopen(url1) as response:
         html = response.read()
     dattod = json.loads(html.decode('utf-8'))
-    with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/' +
-                                sys.argv[1]) as response:
+    with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users') as response:
         html = response.read()
     datuser = json.loads(html.decode('utf-8'))
-    for item in dattod:
-        if int(item['userId']) == int(userid):
-            tasksdone.append(item)
+    tmp_dict = {}
     content = []
-    file1 = "{}.json".format(userid)
-    for task in tasksdone:
-        content.append({"task": task['title'],
-                        "completed": task['completed'],
-                        "username": datuser['username']})
-    tmp_dict = {userid: content}
+    for user in datuser:
+        for item in dattod:
+            content.append({"task": item['title'],
+                        "completed": item['completed'],
+                        "username": user['username']})
+        tmp_dict = {user['id']: content}
+    file1 = "{}.json".format("todo_all_employees")
     with open(file1, 'w') as emplo_file:
         json.dump(tmp_dict, emplo_file)
