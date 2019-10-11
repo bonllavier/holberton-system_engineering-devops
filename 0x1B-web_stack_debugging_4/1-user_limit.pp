8b221abf19eef0770edc095ab82fe5b0db8b427c
javb92 <mentor37_@hotmail.com>
2019-10-11 04:39:57 +0000

    [FIX] lines too long

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,17 +4,19 @@ exec { 'delte line with pattern 1':
   command => "sed -i '/holberton hard/d' /etc/security/limits.conf",
 }
 exec { 'delte line with pattern 2':
-require   => Exec['delte line with pattern 1'],
+  require => Exec['delte line with pattern 1'],
   path    => '/usr/bin:/usr/sbin:/bin',
   command => "sed -i '/holberton soft/d' /etc/security/limits.conf",
 }
 exec { 'config the limit user 1':
-require   => Exec['delte line with pattern 2'],
+  require => Exec['delte line with pattern 2'],
   path    => '/bin/echo:/usr/sbin:/bin',
-  command => "sed -i '55i holberton hard nofile 5000' /etc/security/limits.conf",
+  command => "sed -i '55i holberton hard nofile 5000'
+  /etc/security/limits.conf",
 }
 exec { 'config the limit user 2':
-require   => Exec['config the limit user 1'],
+  require => Exec['config the limit user 1'],
   path    => '/bin/echo:/usr/sbin:/bin',
-  command => "sed -i '56i holberton soft nofile 4000' /etc/security/limits.conf",
+  command => "sed -i '56i holberton soft nofile 4000'
+  /etc/security/limits.conf",
 }
