a4c2f4c8bbd1250180f2c7184cde30d165216e39
javb92 <mentor37_@hotmail.com>
2019-10-11 03:16:19 +0000

    [FIRST]

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Sky is the limit, let's bring that limit higher
+exec { 'delte line with pattern':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => "sed -i '/ULIMIT=/d' /etc/default/nginx",
+}
+exec { 'config the limit in nginx':
+  path    => '/bin/echo:/usr/sbin:/bin',
+  command => "echo \"ULIMIT=\"-n 15000\"\" >> /etc/default/nginx",
+}
