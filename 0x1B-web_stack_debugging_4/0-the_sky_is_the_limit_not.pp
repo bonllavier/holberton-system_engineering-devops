12f0ef5196efb043b636031333161c264192d37b
javb92 <mentor37_@hotmail.com>
2019-10-11 03:27:59 +0000

    [FIX] indentation and service restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Sky is the limit, let's bring that limit higher task
+# Sky is the limit, let's bring that limit higher
 exec { 'delte line with pattern':
   path    => '/usr/bin:/usr/sbin:/bin',
   command => "sed -i '/ULIMIT=/d' /etc/default/nginx",
@@ -6,4 +6,8 @@ exec { 'delte line with pattern':
 exec { 'config the limit in nginx':
   path    => '/bin/echo:/usr/sbin:/bin',
   command => "echo \"ULIMIT=\"-n 15000\"\" >> /etc/default/nginx",
+}
+exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
